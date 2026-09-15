2ebb19484d4b7381958293bd354e1fb53dc44857
Simon Jentzsch <simon@corpus.io>
2026-09-15 10:27:54 +0200

    warn

--- prepare-testdata.mjs
+++ prepare-testdata.mjs
@@ -129,7 +129,7 @@ async function rpc(method, params) {
     const j = await res.json();
     if (j.error) {
         const err = new Error(`${method}: ${JSON.stringify(j.error)}`);
-        console.error('rpc-error:', err.message);
+        console.warn('rpc-error:', err.message);
         err.rpcError = j.error;
         throw err;
     }
